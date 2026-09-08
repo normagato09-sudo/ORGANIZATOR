2f7246075d2e78b27136b70ead9462abbb8d3ae5
Norma <normagato09@gmail.com>
2026-09-08 15:54:46 +0200

    chore: subir version a 2.1.0 para forzar refresco de cache PWA

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * que alguien se quede atascado con una versión vieja.
  */
 
-const CACHE_VERSION = 'v2.0.0';
+const CACHE_VERSION = 'v2.1.0';
 const CACHE_NAME = `organizator-shell-${CACHE_VERSION}`;
 
 // Recursos propios de la app que se pueden precachear con seguridad.
